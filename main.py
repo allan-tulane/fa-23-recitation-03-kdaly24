e2a9eb65a23c4e532758bceadb4fa717e47dd44b
Killian_Daly <kdaly3@tulane.edu>
2023-09-20 17:53:48 -0500

    Answered and added tests for questions 7 8 and 9

--- main.py
+++ main.py
@@ -46,18 +46,40 @@ def pad(x,y):
 
 def quadratic_multiply(x, y):
     # this just converts the result from a BinaryNumber to a regular int
-    return _quadratic_multiply(x,y).decimal_val
+    z = _quadratic_multiply(x,y)
+    return z.decimal_val
 
 def _quadratic_multiply(x, y):
     ### TODO
-    pass
+    x = x.binary_vec
+    y = y.binary_vec
+    s = pad(x, y)
+    x = s[0]
+    y = s[1]
+    if ((binary2int(x).decimal_val <= 1) and (binary2int(y).decimal_val <= 1)):
+        return BinaryNumber(binary2int(x).decimal_val * binary2int(y).decimal_val)
+    else:
+        xsplit = split_number(x)
+        ysplit = split_number(y)
+        xleft = xsplit[0]
+        xright = xsplit[1]
+        yleft = ysplit[0]
+        yright = ysplit[1]
+        val1 = bit_shift((_quadratic_multiply(xleft,yleft)), (len(x)))
+        val2 = bit_shift(_quadratic_multiply(xleft, yright), len(x) // 2)
+        val3 = bit_shift(_quadratic_multiply(xright, yleft), len(x) // 2)
+        val4 = bit_shift(_quadratic_multiply(xright, yright),0)
+
+        return BinaryNumber(val1.decimal_val + val2.decimal_val + val3.decimal_val + val4.decimal_val)
     ###
 
 
     
     
-def test_quadratic_multiply(x, y, f):
+def test_quadratic_multiply():
     start = time.time()
+
+    _quadratic_multiply(BinaryNumber(2), BinaryNumber(2))
     # multiply two numbers x, y using function f
     
     return (time.time() - start)*1000
--- test_main.py
+++ test_main.py
@@ -5,3 +5,4 @@ from main import *
 ## Feel free to add your own tests here.
 def test_multiply():
     assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert quadratic_multiply(BinaryNumber(3), BinaryNumber(2)) == 3*2
